bde7a8f0dd874d720cc842e9bd31675047714c76
utin-francis-peter <u.francispeter@gmail.com>
2022-04-28 17:17:22 +0100

    Second commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const reset = document.querySelector(".reset");
 
 // FUNCTIONS ENVIRONMENT FOR DIFFERENT PURPOSES
 // function that styles dom elements if condition for computation isn't met
-function warningStyles() {
+function addWarningStyles() {
 	warning.classList.remove("hidden");
 	warning.style.color = "red";
 	peopleContainer.style.borderColor = "red";
@@ -22,13 +22,18 @@ function removeWarningStyles() {
 	warning.classList.add("hidden");
 	peopleContainer.style.borderColor = "transparent";
 }
+// function that resets tip and total amount values to zero before new computation is done
+function resetOutputValues() {
+	tipAmount.textContent = "0.00";
+	totalAmount.textContent = "0.00";
+}
 // function that calculates tip amount per person
-function tipAmountCalc(generalBill, numberOfPeople, tipPercent) {
+function calculateTipAmount(generalBill, numberOfPeople, tipPercent) {
 	return ((tipPercent / 100) * generalBill) / numberOfPeople;
 }
 // function that calculates total amount per person
-function totalAmountCalc(generalBill, numberOfPeople, tipPercent) {
-	const computedTipAmount = tipAmountCalc(
+function calculateTotalAmount(generalBill, numberOfPeople, tipPercent) {
+	const computedTipAmount = calculateTipAmount(
 		generalBill,
 		numberOfPeople,
 		tipPercent
@@ -38,11 +43,11 @@ function totalAmountCalc(generalBill, numberOfPeople, tipPercent) {
 	return computedTipAmount + generalBill / numberOfPeople;
 }
 // function that convert DOM elements to numbers and invoke calc functions to compute results
-function toNumberAndTrigger(tipPercent) {
+function TriggerTotalAmountCalc(tipPercent) {
 	const generalBill = Number(billInput.value);
 	const numberOfPeople = Number(peopleInput.value);
-	// invoking totalAmountCalc function which also invoked the tipAmountcalc function
-	const computedTotalAmount = totalAmountCalc(
+	// invoking calculateTotalAmount function which also invoked the calculateTipAmount function
+	const computedTotalAmount = calculateTotalAmount(
 		generalBill,
 		numberOfPeople,
 		tipPercent
@@ -63,45 +68,46 @@ for (let i = 0; i < percentage.length; i++) {
 		// invoking computation function if condition is met
 		if (generalBill && numberOfPeople !== 0) {
 			// resets current tip amount and total amount values before calling the computation function
-			tipAmount.textContent = "0.00";
-			totalAmount.textContent = "0.00";
+			resetOutputValues();
 			//invokes computation function
-			toNumberAndTrigger(tipPercent);
+			TriggerTotalAmountCalc(tipPercent);
 			// invokes removeWarningStyles function since condition for computation execution is met
 			removeWarningStyles();
 		} else {
 			tipAmount.textContent = "0.00";
 			totalAmount.textContent = "0.00";
-			warningStyles();
+			addWarningStyles();
 		}
 	});
 }
 
 // EXECUTING COMPUTATION WHEN CUSTOM TIP PERCENTAGE VALUE IS ENTERED
 for (let i = 0; i < customTip.length; i++) {
-	customTip[i].addEventListener("keydown", function (e) {
-		if (e.key === "Enter") {
-			const customTipPercentage = Number(customTip[i].value);
-			const generalBill = Number(billInput.value);
-			const numberOfPeople = Number(peopleInput.value);
-			// invoking computation function if condition is met
-			if (generalBill && numberOfPeople !== 0) {
-				//invokes computation function
-				toNumberAndTrigger(customTipPercentage);
-				// invokes removeWarningStyles function
-				removeWarningStyles();
-			} else {
-				warningStyles();
-			}
+	customTip[i].addEventListener("input", function () {
+		const customTipPercentage = Number(customTip[i].value);
+		const generalBill = Number(billInput.value);
+		const numberOfPeople = Number(peopleInput.value);
+		// invoking computation function if condition is met
+		if (generalBill && numberOfPeople !== 0) {
+			// resets current tip amount and total amount values before calling the computation function
+			resetOutputValues();
+			//invokes computation function
+			TriggerTotalAmountCalc(customTipPercentage);
+			// invokes removeWarningStyles function
+			removeWarningStyles();
+		} else {
+			// resets current tip amount and total amount values before calling the computation function
+			resetOutputValues();
+			addWarningStyles();
 		}
 	});
 }
 
-// RESETING FIELDS, TEXT-CONTENTS AND VALUES WHEN RESET BUTTON IS CLICKED
+// RESETING INPUT FIELD VALUES AND TEXT-CONTENTS WHEN RESET BUTTON IS CLICKED
 reset.addEventListener("click", function () {
 	for (let i = 0; i < customTip.length; i++) customTip[i].value = "";
 	billInput.value = "";
 	peopleInput.value = "";
-	tipAmount.textContent = "0.00";
-	totalAmount.textContent = "0.00";
+	resetOutputValues();
+	removeWarningStyles();
 });
